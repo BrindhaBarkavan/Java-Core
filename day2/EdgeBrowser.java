63ac408d06a9ea0bda89276c24b9724aac5f0619
BrindhaBarkavan <130645220+BrindhaBarkavan@users.noreply.github.com>
2023-09-25 16:12:46 +0530

    EdgeBrowser accessing Browsers method
    creation of methods ,class and executing this methods using another class from outside

---
+++ day2/EdgeBrowser.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Browser object1=new Browser();
+		 System.out.println(object1.launchBrowser("Opera"));
+		 object1.loadUrl();
+	}
+
+}
